36bf18bb216cdb5a806abbe74ad2bd0d1fdefc72
hridoy667 <104277877+hridoy667@users.noreply.github.com>
2024-12-23 19:22:50 +0600

    Update urls.py

--- symptocare/urls.py
+++ symptocare/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('landing/', include('Landing_page.urls')),
+    path('', RedirectView.as_view(url='/landing/', permanent=True)),
     path('su/', include('signup.urls')),
     path('dashbord/', include('dashbord.urls')),
     path('signin/', include('signin.urls')),
