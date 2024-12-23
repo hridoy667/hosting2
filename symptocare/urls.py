5a18354e057505c5fc88fdbeb2e229c5e499ce9f
hridoy667 <104277877+hridoy667@users.noreply.github.com>
2024-12-23 22:33:27 +0600

    Update urls.py

--- symptocare/urls.py
+++ symptocare/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from Landing_page.views import landing
 from Landing_page import views
+from Landing_page.views import landing
 
 urlpatterns = [
     path('admin/',admin.site.urls),
